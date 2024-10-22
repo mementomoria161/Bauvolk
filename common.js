e36d54feb0c3546e260f9922490cc1cdef1d4ff4
GEFÄNGNISHEFTE <gefaengnishefte@riseup.net>
2024-10-22 16:20:04 +0200

    upload

--- common.js
+++ common.js
@@ -193,17 +193,6 @@ function init_pwa() {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// HEIGHTFIX
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-window.addEventListener('resize', () => {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
-
 // SCROLLBAR FOR INDEX
 
 function init_scrollbar() {
